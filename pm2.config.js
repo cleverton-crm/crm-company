dbe681d53ae37bb417f2dbaf0fa54bc8a7a29ff4
Alexander Hunter <icehuntmen@gmail.com>
2021-12-22 17:26:01 +0300

    Fix port and name

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'crm-users',
+      name: 'crm-company',
       script: './dist/main.js',
       watch: false,
       wait_ready: true,
       stop_exit_codes: [0],
       env: {
-        PORT: 5011,
+        PORT: 5015,
       },
       env_production: {
         NODE_ENV: 'production',
