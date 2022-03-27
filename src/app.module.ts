26ce4b8a807d2239b70904a3cc58d38ea3d16fdd
Alexander Shumilin <icehuntmen@gmail.com>
2022-03-27 14:27:59 +0300

    Libs for classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,43 @@
 import { Module } from '@nestjs/common';
-import { CompanyController } from './controllers/company.controller';
-import { CompanyService } from './services/company.service';
+import { ConfigService } from './config/config.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
-import { JwtConfigService } from './services/jwt.services';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MongoConfigService } from './services/mongo.service';
+
 import {
+  ActivityProviderSchema,
+  CarsProviderSchema,
+  ClientsProviderSchema,
   CompanyProviderSchema,
+  DealsProviderSchema,
+  LeadClientsProviderSchema,
   LeadCompanyProviderSchema,
   ListCompanyProviderSchema,
-} from './providers/company.provider';
-import { ConfigService } from './config/config.service';
-import { ClientsProviderSchema, LeadClientsProviderSchema } from './providers/clients.provider';
-import { ClientsController } from './controllers/clients.controller';
-import { ClientService } from './services/clients.service';
-import { CarsProviderSchema } from './providers/cars.provider';
-import { CarsController } from './controllers/cars.controller';
-import { CarsService } from './services/cars.service';
-import { DealsService } from './services/deals.service';
-import { LeadsController } from './controllers/leads.controller';
-import { LeadsService } from './services/leads.service';
-import { DealsController } from './controllers/deals.controller';
-import { DealsProviderSchema } from './providers/deals.provider';
-import { StatusDealsProviderSchema } from './providers/status-deals.provider';
-import { StatusDealsService } from './services/status-deals.service';
-import { StatusDealsController } from './controllers/status-deals.controller';
-import { ProfileProviderSchema } from './providers/profile.provider';
-import { ActivityProviderSchema } from './providers/activity.provider';
-import { ActivityService } from './services/activity.service';
-import { ActivityController } from './controllers/activity.controller';
-import { ParkCompanyProviderSchema } from './providers/park.provider';
+  ParkCompanyProviderSchema,
+  ProfileProviderSchema,
+  StatusDealsProviderSchema,
+} from './providers';
+import {
+  ActivityController,
+  CarsController,
+  ClientsController,
+  CompanyController,
+  DealsController,
+  LeadsController,
+  StatusDealsController,
+} from './controllers';
+
+import {
+  ActivityService,
+  CarsService,
+  ClientService,
+  DealsService,
+  ImportService,
+  JwtConfigService,
+  LeadsService,
+  MongoConfigService,
+  StatusDealsService,
+} from './services';
 
 @Module({
   imports: [
@@ -76,6 +83,7 @@ import { ParkCompanyProviderSchema } from './providers/park.provider';
     LeadsService,
     StatusDealsService,
     ActivityService,
+    ImportService,
   ],
   exports: [ConfigService],
 })
